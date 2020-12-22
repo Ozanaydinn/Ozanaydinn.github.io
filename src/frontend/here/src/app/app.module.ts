072e9d7c3583a892156119a3bc030cc7b359a4ea
Yüce <yuceyuce99@gmail.com>
2020-12-22 23:49:41 +0300

    Add empty main page and a page-not-found page

--- src/frontend/here/src/app/app.module.ts
+++ src/frontend/here/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
