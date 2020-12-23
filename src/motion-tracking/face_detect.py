2dd6a2ffb5d763ac7c23c7927e78c69a5099f83f
Mert Aslan <aslan-mert99@hotmail.com>
2020-12-23 19:32:18 +0300

    Add initial motion-tracking

---
+++ src/motion-tracking/face_detect.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+
+def init_model():
+    model = cv2.dnn.readNetFromCaffe("deploy.prototxt.txt",
+                "res10_300x300_ssd_iter_140000.caffemodel")
+
+    return model
+
+def find_faces(frame, model):
+    
+    # grab the frame dimensions and convert it to a blob
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
+        (300, 300), (104.0, 177.0, 123.0))
+
+    # pass the blob through the network and obtain the detections and
+    # predictions
+    faces = []
+    model.setInput(blob)
+    detections = model.forward()
+
+    # loop over the detections
+    for i in range(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with the
+        # prediction
+        confidence = detections[0, 0, i, 2]
+        
+        if confidence < 0.5 :
+            continue
+        # compute the (x, y)-coordinates of the bounding box for the
+        # object
+        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+        (startX, startY, endX, endY) = box.astype("int")
+
+        faces.append([startX, startY, endX, endY])
+
+    return faces
+
+def draw_faces_on_frame(frame, faces):
+
+    for startX, startY, endX, endY in faces:
+
+        cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 0, 255), 3)
