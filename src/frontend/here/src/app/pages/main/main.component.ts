5c55fe65b4a326da2d090b3da2c4a9778f5b267a
Yüce <yuceyuce99@gmail.com>
2020-12-23 19:13:38 +0300

    Fix opening video stream twice

--- src/frontend/here/src/app/pages/main/main.component.ts
+++ src/frontend/here/src/app/pages/main/main.component.ts
@@ -9,10 +9,12 @@ export class MainComponent implements OnInit {
 
   video: HTMLVideoElement;
   constraints = { audio: true, video: true };
+  videoOn: boolean
+
   constructor() { }
 
   ngOnInit(): void {
-
+    this.videoOn = false;
   }
 
   startVideo(): void {
@@ -21,9 +23,11 @@ export class MainComponent implements OnInit {
       stream => {
         this.video.srcObject = stream
         console.log("Streaming video")
+        this.videoOn = true
       },
       error => {
         console.log('Error: ' + error);
+        this.videoOn = false;
       });
   }
 
@@ -31,5 +35,54 @@ export class MainComponent implements OnInit {
     (<MediaStream>this.video.srcObject).getTracks().forEach( (track) => {
       track.stop();
     })
+    this.videoOn = false;
   }
+
+  foo(): void {
+    var socket = io('http://localhost:5000');
+
+    socket.on('connect', function(){
+        console.log("Connected...!", socket.connected)
+    });
+
+    const video = document.querySelector("#videoElement");
+
+    video.width = 500; 
+    video.height = 375; ;
+
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+        .then(function (stream) {
+            video.srcObject = stream;
+            video.play();
+        })
+        .catch(function (err0r) {
+            console.log(err0r)
+            console.log("Something went wrong!");
+        });
+    }
+
+    let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+    let dst = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+    let cap = new cv.VideoCapture(video);
+
+    const FPS = 22;
+
+    setInterval(() => {
+        cap.read(src);
+
+        var type = "image/png"
+        var data = document.getElementById("canvasOutput").toDataURL(type);
+        data = data.replace('data:' + type + ';base64,', ''); //split off junk at the beginning
+
+        socket.emit('image', data);
+    }, 10000/FPS);
+
+
+    socket.on('response_back', function(image){
+        const image_id = document.getElementById('image');
+        image_id.src = image;
+    });
+  }
+
 }
