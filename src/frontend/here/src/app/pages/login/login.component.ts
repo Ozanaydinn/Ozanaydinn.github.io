4e6f7243e265fd21c9cc7b7d61ad7aa4372c284c
burakokumus <burakokumus98@gmail.com>
2021-01-14 19:00:46 +0300

    Init login page

---
+++ src/frontend/here/src/app/pages/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.sass']
+})
+export class LoginComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
