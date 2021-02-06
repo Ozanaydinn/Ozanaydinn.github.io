60b98d0de950ba58fdb08c95d02f828506fa2eb7
burakokumus <burakokumus98@gmail.com>
2021-02-06 21:55:56 +0300

    Update main page

--- src/frontend/here/src/app/pages/main/main.component.ts
+++ src/frontend/here/src/app/pages/main/main.component.ts
@@ -6,9 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.sass'],
 })
 export class MainComponent implements OnInit {
-
+  upcomingClasses: any[];
   constructor() {}
 
   ngOnInit(): void {
+    this.upcomingClasses = [
+      { // will be fetched
+        name: "ML",
+        code: "CS464",
+        time: "09.30",
+        link:""
+      },
+      {
+        name: "PMBOK",
+        code: "CS413",
+        time: "13.30",
+        link:""
+      }
+    ];
   }
+
+  
 }
