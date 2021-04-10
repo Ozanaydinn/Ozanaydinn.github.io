ec7fa5d2f2a83a6ec551d6d03b6f096e6fbb834c
Ozan Aydın <ozanaydinn07@gmail.com>
2021-04-10 15:34:29 +0300

    change folder name "analyze-image" to "analyze_image"

--- src/backend/image.py
+++ src/backend/image.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource, reqparse
 import base64
 from PIL import Image
+from ta.motion_tracking.analyze_image import analyze_image
 
 class SendImage(Resource):
     def post(self):
@@ -9,13 +10,11 @@ class SendImage(Resource):
         # sbuf.write(data)
 
         # decode and convert into image
-        """
         data = data.replace("data:image/png;base64,", "")
         im_bytes = base64.b64decode(data)   # im_bytes is a binary image
         im_file = io.BytesIO(im_bytes)  # convert image to file-like object
         pimg = Image.open(im_file)   # img is now PIL Image object
-        pimg.show()
-        """
+        print("Done converting the image")
         # converting RGB to BGR, as opencv standards
         # frame = cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
 
--- src/backend/ta/motion_tracking/analyze_image.py
+++ src/backend/ta/motion_tracking/analyze_image.py
@@ -7,7 +7,7 @@ def analyze_image(frame):
 
     
     hand_result = recognize_hand_gesture(frame)
-    head_pose_result = estimate_head_pose(frame)
+    estimate_head_pose(frame)
     phone_result = search_phone(frame)
 
-    print("Hand_result: " + hand_result + " Head Pose Result: " + head_pose_result + " Phone result: " + phone_result)
+    print("Hand_result: " + hand_result + " Phone result: " + phone_result)
