039f4e1edd92f1b2ca2709f40eccf43d2f276a33
mert <aslan-mert99@hotmail.com>
2021-05-01 17:45:21 +0300

    Update head model pre-load

--- src/ta/app/head/analyze_head.py
+++ src/ta/app/head/analyze_head.py
@@ -1,15 +1,8 @@
-from head_pose_estimation import HeadPoseEstimation
-from face_detect import FaceDetect
-from face_landmark_detection import FaceLandmark
+from head_pose_estimation import estimate_head_pose
 
 def analyze_head(frame):
 
-    face_detector = FaceDetect()
-    face_landmark_detector = FaceLandmark()
-    head_pose_analyzer = HeadPoseEstimation(frame, face_detector, face_landmark_detector)
-
-
-    head_pose_result = head_pose_analyzer.estimate_head_pose()
+    head_pose_result = estimate_head_pose(frame)
 
     result_dict = {
         "head_pose_result": head_pose_result
--- src/ta/app/head/face_detect.py
+++ src/ta/app/head/face_detect.py
@@ -1,47 +1,45 @@
 import cv2
 import numpy as np
 
-class FaceDetect:
+def init_model():
+    model = cv2.dnn.readNetFromCaffe("models/deploy.prototxt.txt",
+                "models/res10_300x300_ssd_iter_140000.caffemodel")
+    print(type(model))
 
-    def init_model(self):
-        model = cv2.dnn.readNetFromCaffe("models/deploy.prototxt.txt",
-                    "models/res10_300x300_ssd_iter_140000.caffemodel")
-        print(type(model))
+    return model
 
-        return model
+def find_faces(frame, model):
+    
+    # grab the frame dimensions and convert it to a blob
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
+        (300, 300), (104.0, 177.0, 123.0))
 
-    def find_faces(self, frame, model):
+    # pass the blob through the network and obtain the detections and
+    # predictions
+    faces = []
+    model.setInput(blob)
+    detections = model.forward()
+
+    # loop over the detections
+    for i in range(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with the
+        # prediction
+        confidence = detections[0, 0, i, 2]
         
-        # grab the frame dimensions and convert it to a blob
-        (h, w) = frame.shape[:2]
-        blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
-            (300, 300), (104.0, 177.0, 123.0))
+        if confidence < 0.7 :
+            continue
+        # compute the (x, y)-coordinates of the bounding box for the
+        # object
+        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+        (startX, startY, endX, endY) = box.astype("int")
 
-        # pass the blob through the network and obtain the detections and
-        # predictions
-        faces = []
-        model.setInput(blob)
-        detections = model.forward()
+        faces.append([startX, startY, endX, endY])
 
-        # loop over the detections
-        for i in range(0, detections.shape[2]):
-            # extract the confidence (i.e., probability) associated with the
-            # prediction
-            confidence = detections[0, 0, i, 2]
-            
-            if confidence < 0.7 :
-                continue
-            # compute the (x, y)-coordinates of the bounding box for the
-            # object
-            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
+    return faces
 
-            faces.append([startX, startY, endX, endY])
+def draw_faces_on_frame(frame, faces):
 
-        return faces
+    for startX, startY, endX, endY in faces:
 
-    def draw_faces_on_frame(self, frame, faces):
-
-        for startX, startY, endX, endY in faces:
-
-            cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 0, 255), 3)
+        cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 0, 255), 3)
